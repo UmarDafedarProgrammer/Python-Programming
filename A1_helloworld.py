3a3deee8f7f79f94bb031d05307623159645c13b
Umar Dafedar <108915275+UmarDafedarProgrammer@users.noreply.github.com>
2025-07-27 13:44:59 +0530

    Clearing all the Old Files

--- A1_helloworld.py
+++
@@ -1,6 +0,0 @@
-print('Hello, World!')
-
-print(1 + 2)
-print(6*7)
-print()
-print("End!","Second String", 3)
--- A3_strings.py
+++
@@ -1,9 +0,0 @@
-print("Today, we are going to learn about strings.")
-print('Single Quote can also be used for strings.')
-print("Python's strings")
-print('We can also use "double quote"')
-print("hello"+" world")
-
-name = input("Please, enter your name. ")
-greeting = "Hello"
-print(greeting + ' ' + name)
